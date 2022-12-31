1100513830e36add6baf6733306a36519b2f7309
Suyash Udchan <suyash.udchan@gmail.com>
2022-12-31 10:14:53 +0530

    Second Commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,12 +10,12 @@ def post_list(request):
      posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
      return render(request, 'post_list.html', {'posts': posts})
 
-
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -29,7 +29,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
